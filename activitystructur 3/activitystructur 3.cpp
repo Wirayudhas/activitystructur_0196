58827e0d8daa7a260559d26cf0436b0b93c607ee
Wirayudhas <arturwirayuda@gmail.com>
2024-04-02 12:13:39 +0700

    Menambahkan struct DetailAlamat

--- activitystructur 3/activitystructur 3.cpp
+++ activitystructur 3/activitystructur 3.cpp
@@ -8,37 +8,3 @@ struct DetailAlamat {
 	string kota;  
 };
 
-struct Mahasiswa {
-	string nim;   
-	string nama; 
-	DetailAlamat alamat;  
-	int umur;    
-};
-
-int main() {
-	Mahasiswa mhs;
-
-	cout << "Masukkan NIM: ";
-	getline(cin, mhs.nim);
-
-	cout << "Masukkan Nama: ";
-	getline(cin, mhs.nama);
-
-	cout << "Masukkan Alamat Desa: ";
-	getline(cin, mhs.alamat.desa);
-
-	cout << "Masukkan Alamat Kota: ";
-	getline(cin, mhs.alamat.kota);
-
-	cout << "Masukkan Umur: ";
-	cin >> mhs.umur;
-
-	cout << endl;
-	cout << "\n NIM: " << mhs.nim;
-	cout << "\n Nama: " << mhs.nama;
-	cout << "\n Nama Desa" << mhs.alamat.desa;
-	cout << "\n Nama Kota: " << mhs.alamat.kota;
-	cout << "\n Umur: " << mhs.umur;
-
-	return 0;
-}
